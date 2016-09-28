bf4643415d148ae782d33b8ff4b364ac63a43661
hck <art.hacker@ya.ru>
2016-09-28 15:30:24 +0300

    fix provision
    add apt-get update
    add -rf flags

--- provision.sh
+++ provision.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
 sudo apt-get install libapache2-mod-php5 -y # PHP5+Apache
 sudo a2enmod rewrite # Enabling mod_rewrite
 sudo a2enmod headers # Enabling mod_headers
 sudo aptitude install php5-gd # GD lib
 
 #Apache config
-rm /etc/apache2/sites-available/*
-rm /etc/apache2/sites-enabled/*
+rm -rf /etc/apache2/sites-available/*
+rm -rf /etc/apache2/sites-enabled/*
 ln -s /vagrant/apache2/site.conf /etc/apache2/sites-available/site.conf
 ln -s /vagrant/apache2/site.conf /etc/apache2/sites-enabled/site.conf
 
